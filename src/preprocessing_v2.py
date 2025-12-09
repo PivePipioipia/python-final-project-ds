b4e78b3ada6aee2bc9d9333ba0baeb5e7ded6cfc
PivePipioipia <Yennguyen140205@gmail.com>
2025-12-09 15:41:04 +0700

    add: streamlit ui

--- src/preprocessing_v2.py
+++ src/preprocessing_v2.py
@@ -253,13 +253,10 @@ class DataPreprocessorV2(BasePreprocessor):
         df = self._add_derived_features(df)
         df = self._select_features_for_modeling(df)
         
-        # Reorder columns to match fit
-        missing_cols = set(self.feature_names) - set(df.columns)
-        for c in missing_cols:
-            df[c] = 0 # Điền 0 cho feature thiếu (e.g. Genre mới lạ)
+        if self.scaler:
+             # Ensure columns match exactly what scaler was fitted on
+            df = df.reindex(columns=self.feature_names, fill_value=0)
             
-        df = df[self.feature_names] 
-        
         X = self.scaler.transform(df)
         return X, y
 
